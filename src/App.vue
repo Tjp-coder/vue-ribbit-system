9fbed0d9b44b0f9b675f94629404d3591568c3a0
Tjp-coder <838219014@qq.com>
2024-06-23 02:00:07 +0800

    项目初始化入门提交

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-
+// @/符号只起联想提示作用，而不是实际路径转换，真正路径转换由vite.config.js中的defineConfig函数中的 resolve.alias配置完成
+import dd from "@/main.js"
 </script>
 
 <template>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath, URL} from "node:url";
 //此文件是vite的配置文件
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    // 实际的路径转换  @  -> src
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
 })
