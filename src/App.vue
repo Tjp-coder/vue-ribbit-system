89409d680b80c9a5c33ce9d2a9c6db163cf253f8
Tjp-coder <838219014@qq.com>
2024-06-23 11:12:06 +0800

    添加了主题色定制

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script>
 // @/符号只起联想提示作用，而不是实际路径转换，真正路径转换由vite.config.js中的defineConfig函数中的 resolve.alias配置完成
-import dd from "@/main.js"
+
 </script>
 
 <template>
-
+  <el-button type="primary">我是 ElButton</el-button>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,42 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from "node:url";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+
 //此文件是vite的配置文件
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    // 实际的路径转换  @  -> src
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+    plugins: [vue(),
+        //自动导入和组件  配置插件
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            // 配置elementPlus 使用 sass样式配色系统
+            resolvers: [ElementPlusResolver({importStyle : "sass"})],
+        }),
+    ],
+    resolve: {
+        // 实际的路径转换  @  -> src
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
+    },
+
+    //css配置
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 自动导入定制化样式文件进行样式覆盖
+                additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+            }
+        }
     }
-  },
 })
