9142331cdd32fac7e5b20fb450e654bbb79e0dd9
Tjp-coder <838219014@qq.com>
2024-06-24 11:23:06 +0800

    吸顶式导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,13 +2,15 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from "@/views/Layout/components/LayoutFixed.vue";
 </script>
 
 <template>
-  <LayoutNav />
-  <LayoutHeader />
-<!--  路由出口-->
-  <RouterView />
+  <LayoutFixed/>
+    <LayoutNav/>
+    <LayoutHeader/>
+    <!--  路由出口-->
+    <RouterView/>
 
-  <LayoutFooter />
+    <LayoutFooter/>
 </template>
