52f30c5f05cc2d0bed4c787d38e50c5a8c124e8e
Tjp-coder <838219014@qq.com>
2024-07-04 17:47:42 +0800

    面板组件复用，通过props和slot传参

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,6 +5,7 @@ import HomeNewGoods from "@/views/Home/components/HomeNewGoods.vue";
 import HomeCategory from "@/views/Home/components/HomeCategory.vue";
 import HomeBanner from "@/views/Home/components/HomeBanner.vue";
 import HomeProduct from "@/views/Home/components/HomeProduct.vue";
+import HomePanel from "@/views/Home/components/HomePanel.vue";
 </script>
 
 <template>
@@ -18,5 +19,18 @@ import HomeProduct from "@/views/Home/components/HomeProduct.vue";
   <HomeNewGoods />
   <HomeHotRecommend />
   <HomeProduct />
+
+<!--  测试面板组件-->
+  <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+    <div>
+      我是新鲜好物插槽的内容
+    </div>
+  </HomePanel>
+
+  <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
+    <div>
+      我是人气推荐插槽的内容
+    </div>
+  </HomePanel>
 </template>
 
