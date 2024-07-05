2dac09610fa1f6f2bd68416d93b06a0041e30ae1
Tjp-coder <838219014@qq.com>
2024-07-05 16:00:51 +0800

    二级分类-搭建商品详情页面，添加详情跳转路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,42 @@
 // createRouter:创建router实例对象
 //createWebHistory:创建history模式的路由
-import {createRouter,createWebHistory} from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes:[
+    routes: [
         {
             path: '/',
             component: Layout,
             children: [
                 {
                     path: "",
-                    component: ()=>import('@/views/Home/index.vue')
+                    component: () => import('@/views/Home/index.vue')
                 },
                 {
                     path: "category/:id",
-                    component: ()=>import('@/views/Category/index.vue')
+                    component: () => import('@/views/Category/index.vue')
                 },
                 {
                     path: 'category/sub/:id',
-                    component: ()=>import('@/views/SubCategory/index.vue')
+                    component: () => import('@/views/SubCategory/index.vue')
                 },
+                {
+                    path: 'detail/:id',
+                    component: () => import('@/views/Detail/index.vue')
+                }
             ]
         },
         {
             path: '/login',
             component: Login,
         }
-    ]
+    ],
+    scrollBehavior() {
+        return {top: 0}
+    }
 })
 
 export default router
