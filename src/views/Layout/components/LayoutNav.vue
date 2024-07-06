ef5b0c9bafda606348d68662e02e2891dbdd0a34
Tjp-coder <838219014@qq.com>
2024-07-06 12:38:42 +0800

    登录-退出登录业务实现

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,8 +1,17 @@
 <script setup>
 
-import router from "@/router/index.js";
 import {useUserStore} from "@/stores/userStore.js";
+import {useRouter} from "vue-router";
 const userStore = useUserStore();
+const router = useRouter()
+const confirm = () => {
+  console.log('用户要退出登录了')
+  // 退出登录业务逻辑实现
+  // 1.清除用户信息 触发action
+  userStore.clearUserInfo()
+  // 2.跳转到登录页
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -12,7 +21,7 @@ const userStore = useUserStore();
         <template v-if="userStore.userInfo">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm  @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
