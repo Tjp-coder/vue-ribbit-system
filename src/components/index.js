c8a7e1bb3bc705bd59ed9877c865a9d729fcd675
Tjp-coder <838219014@qq.com>
2024-07-06 21:09:31 +0800

    主页-增加内容反馈-使用富文本编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -3,15 +3,10 @@
 import ImageView from './ImageView/index.vue'
 import Sku from './XtxSku/index.vue'
 
-import FeedbackDialog from '@/components/Dialog/DialogTest.vue';
-import {QuillEditor} from "@vueup/vue-quill";
-
 export const componentPlugin = {
   install (app) {
     // app.components('组件名字'，组件配置对象)
     app.component('XtxImageView', ImageView)
     app.component('XtxSku', Sku)
-
-
   }
 }
